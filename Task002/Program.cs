f023b63c6e387e7ac89f2eeaaf62bc84953f5174
maksimrepkov <maksimrepkov@yandex.ru>
2023-11-03 10:35:11 +0300

    Добавил решение задачи 56 из ДЗ к семинару 8

---
+++ Task002/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 56.
+// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Функция заполнения массива рандомно числами от 1 до 9
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Функция вывода двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumOfRow(int[,] matrix, int row)
+{
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum = sum + matrix[row, j];
+    }
+    return sum;
+}
+
+int[] MinimumSumRow(int[,] matrix)
+{
+    int minSum = SumOfRow(matrix, 0);
+    int minRow = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = SumOfRow(matrix, i);
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minRow = i;
+        }
+    }
+    return new int[] { minRow, minSum };
+}
+
+int[,] my2DArray = new int[3, 4];
+FillArrayRandom(my2DArray);
+PrintArray(my2DArray);
+MinimumSumRow(my2DArray);
+Console.WriteLine();
+int[] myArray = new int[2];
+myArray = MinimumSumRow(my2DArray);
+Console.WriteLine($"{string.Join(", ", myArray)}");
---
+++ Task003/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 58.
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Функция вывода двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] firstMatrix = new int[3, 4];
+int[,] secondMatrix = new int[3, 4];
+FillArrayRandom(firstMatrix);
+PrintArray(firstMatrix);
+Console.WriteLine();
+FillArrayRandom(secondMatrix);
+PrintArray(secondMatrix);
