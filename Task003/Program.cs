879b88e9d8d094395c4da3bedfff1b64922a6565
maksimrepkov <maksimrepkov@yandex.ru>
2023-11-04 12:13:01 +0300

    Добавил решение задачи 58 из ДЗ к семинару 8

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,34 +1,70 @@
 ﻿// Задача 58.
 // Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
-void FillArrayRandom(int[,] array)
+void FillMatrixRandom(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(1, 10);
+            matrix[i, j] = new Random().Next(1, 10);
         }
     }
 }
 
-// Функция вывода двумерного массива
-void PrintArray(int[,] array)
+// Метод для вывода двумерного массива (матрицы)
+void PrintMatrix(int[,] matrix)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]}\t");
+            Console.Write($"{matrix[i, j]}\t");
         }
         Console.WriteLine();
     }
 }
 
-int[,] firstMatrix = new int[3, 4];
-int[,] secondMatrix = new int[3, 4];
-FillArrayRandom(firstMatrix);
-PrintArray(firstMatrix);
+// Метод для проверки, возможно ли умножения двух матриц matrixA и matrixB
+void MultiplyIfPossible(int[,] matrixA, int[,] matrixB)
+{
+    int[,] matrixC = new int [matrixA.GetLength(0), matrixA.GetLength(1)];
+    Console.WriteLine();
+    if (matrixA.GetLength(1) != matrixB.GetLength(0))
+    {
+        Console.WriteLine("It is impossible to multiply.");
+    }
+    else
+    {
+    matrixC = MatrixMultiplication(matrixA, matrixB);
+    PrintMatrix(matrixC);
+    }
+}
+
+// Метод для умножения двух матриц matrixA и matrixB. 
+int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    int[,] resultMatrix = new int [matrixA.GetLength(0), matrixB.GetLength(1)];
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixB.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                resultMatrix[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+    return resultMatrix;
+}
+
+int[,] firstMatrix = new int[4, 4];
+int[,] secondMatrix = new int[4, 4];
+Console.WriteLine("MatrixA:");
+FillMatrixRandom(firstMatrix);
+PrintMatrix(firstMatrix);
 Console.WriteLine();
-FillArrayRandom(secondMatrix);
-PrintArray(secondMatrix);
+Console.WriteLine("MatrixB:");
+FillMatrixRandom(secondMatrix);
+PrintMatrix(secondMatrix);
+MultiplyIfPossible(firstMatrix, secondMatrix);
