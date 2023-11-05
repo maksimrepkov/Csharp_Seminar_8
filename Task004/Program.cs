06ccef858cbcec08f2884707cc2fb067db397ef2
maksimrepkov <maksimrepkov@yandex.ru>
2023-11-05 19:44:35 +0300

    Добавил изменения в решение задачи 60 из ДЗ к семинару 8

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -5,9 +5,7 @@
 // Метод для заполнения трехмерного массива неповторяющимися двузначными целыми числами
 void Fill3DMatrixRandom(int[,,] matrix)
 {
-
     int number = 10;
-
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
@@ -17,10 +15,10 @@ void Fill3DMatrixRandom(int[,,] matrix)
                 matrix[i, j, k] = number;
                 number++;
             }
-
         }
-      
+
     }
+
 }
 
 // Метод для вывода двумерного массива (матрицы)
@@ -39,7 +37,20 @@ void PrintMatrix(int[,,] matrix)
     Console.WriteLine();
 }
 
-int[,,] my3DMatrix = new int[5, 5, 5];
-Console.WriteLine("my3DMatrix:");
-Fill3DMatrixRandom(my3DMatrix);
-PrintMatrix(my3DMatrix);
+Console.WriteLine("Введите размер 3-мерного массива по оси X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер 3-мерного массива по оси Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер 3-мерного массива по оси Z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int[,,] my3DMatrix = new int[x, y, z];
+
+if (x * y * z <= 90)
+{
+    Console.WriteLine("my3DMatrix:");
+    Fill3DMatrixRandom(my3DMatrix);
+    PrintMatrix(my3DMatrix);
+}
+else
+    Console.WriteLine("Трехмерный массив с такими размерами нельзя заполнить неповторяющимися двузначными числами!");
