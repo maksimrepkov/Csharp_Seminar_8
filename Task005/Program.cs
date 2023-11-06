2c5e5f7d32db75f0c0c6b579827a96fb89b4976f
maksimrepkov <maksimrepkov@yandex.ru>
2023-11-06 19:10:09 +0300

    Добавил решение задачи 62 из ДЗ к семинару 8

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача № 62.
+// Заполните спирально массив 4 на 4.
+
+int len = 4;
+int[,] myArray = new int[len, len];
+FillArraySpiral(myArray, len);
+PrintArray(myArray);
+
+// Метод заполнения массива по спирали начиная с 1
+void FillArraySpiral(int[,] array, int n)
+{
+    int i = 0, j = 0;
+    int value = 1;
+    for (int e = 0; e < n * n; e++)
+    {
+        int k = 0;
+        do { array[i, j++] = value++; } while (++k < n - 1);
+        for (k = 0; k < n - 1; k++) array[i++, j] = value++;
+        for (k = 0; k < n - 1; k++) array[i, j--] = value++;
+        for (k = 0; k < n - 1; k++) array[i--, j] = value++;
+        ++i; ++j;
+        n = n < 2 ? 0 : n - 2;
+    }
+}
+
+// Метод для вывода двумерного массива в терминал
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+            {
+                Console.Write("0" + array[i, j]);
+                Console.Write(" ");
+            }
+            else Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
